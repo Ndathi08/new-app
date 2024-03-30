72a349b3ac42dc9180f22df37962fda6a5b7bf0a
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-03-30 20:53:49 +0200

    Login and signup components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import PropDrilling from './Components/PropDrilling';
 import Perfumes from './Components/Perfumes';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div className="App">
       <PropDrilling/>
       <Perfumes/>
+      <Signup/> 
+      <Login/>
     </div>
   );
 }
